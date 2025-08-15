6d9747d74e04a0d4643eb2a553e613aeeaca4239
Trunkv <dochitrungx@gmail.com>
2025-08-16 00:35:06 +0700

    Fix: hiển thị heap Eager Prim đúng dữ liệu

--- Kruskal.py
+++ Kruskal.py
@@ -461,7 +461,7 @@ elif mode=="Compare":
         st.subheader("Eager Prim")
         draw_graph(G2, data["pos"], steps2[step_eager], "Eager")
         st.write("Heap:")
-        show_heap(steps2[step_eager][4])
+        show_heap(steps2[step_eager][2])  # heap ở [2] cho Eager
 
 # --- Large Graph Compare với lựa chọn Sparse / Dense ---
 elif mode == "Large Graph Compare":
